502878af7a136bbf46c29a94510343d8841c138e
Mark Klinitskii <mark23klin@mail.ru>
2025-12-04 15:01:26 +0300

    Lesson 28 - Generics

--- lessons/src/main/kotlin/lesson_25_working_with_data/product/Store.kt
+++ lessons/src/main/kotlin/lesson_25_working_with_data/product/Store.kt
@@ -1,17 +1,30 @@
 package lesson_25_working_with_data.product
 
-import lesson_25_working_with_data.profile.ProfileRepository
 
 fun main() {
     val products = ProductsRepository.products
 
-    var filtered = filter(products) { it.productPrice > 500 }
-    filtered = filter(filtered) { it.productRating > 4 }
-    filtered = filter(filtered) { it.productCategory == ProductCategory.SPORTS }
+    val filtered = filter(products) {
+        it.productCategory == ProductCategory.CLOTHING
+    }
+    val transformed = transform(filtered) {
+        it.copy(productPrice = it.productPrice * 2)
+    }
+    val productsName = transform(transformed) {
+        "${it.id} - ${it.productName} - ${it.productPrice}"
+    }
 
-    for (product in filtered) {
+    for (product in productsName) {
         println(product)
     }
+}
+
+fun <T> transform (products: List<ProductCard>, operation: (ProductCard) -> T): List<T> {
+    val result = mutableListOf<T>()
+    for (productsCard in products) {
+        result.add(operation(productsCard))
+    }
+    return result
 }
 
 fun filter(products: List<ProductCard>, isSuitable: (ProductCard) -> Boolean): List<ProductCard> {
--- lessons/src/main/kotlin/lesson_25_working_with_data/profile/Profile.kt
+++ lessons/src/main/kotlin/lesson_25_working_with_data/profile/Profile.kt
@@ -8,9 +8,19 @@ fun main() {
     filtered = filter(filtered) { it.firstName.startsWith("A") }
     filtered = filter(filtered) { it.age < 30 }
 
-    for (person in filtered) {
+    val transformed = transform(filtered) { it.copy(age = it.age + 1) }
+
+    for (person in transformed) {
         println(person)
     }
+}
+
+fun <T> transform(profiles: List<Person>, operation: (Person) -> T): List<T> {
+    val result = mutableListOf<T>()
+    for (person in profiles) {
+        result.add(operation(person))
+    }
+    return result
 }
 
 fun filter(profiles: List<Person>, isSuitable: (Person) -> Boolean): List<Person> {
