3b62ebe5a03f5bc83804347ff4470841f76e21f8
Mark Klinitskii <mark23klin@mail.ru>
2025-12-09 14:28:35 +0300

    Lesson 65 - Create GUI

---
+++ lessons/src/main/kotlin/lesson_65_coroutines/callbacks/Display.kt
@@ -0,0 +1,36 @@
+package lesson_65_coroutines.callbacks
+
+import java.awt.BorderLayout
+import java.awt.Dimension
+import javax.swing.JButton
+import javax.swing.JFrame
+import javax.swing.JLabel
+import javax.swing.JPanel
+import javax.swing.JScrollPane
+import javax.swing.JTextArea
+
+object Display {
+
+    private val infoArea = JTextArea().apply {
+        isEditable = false
+    }
+
+    private val loadButton = JButton("Load Book")
+    private val TimerLabel = JLabel("Time: 00:00")
+    private val topPanel = JPanel(BorderLayout()).apply {
+        add(TimerLabel, BorderLayout.WEST)
+        add(loadButton, BorderLayout.EAST)
+    }
+
+
+    private val mainFrame = JFrame("Book and Author info").apply {
+        layout = BorderLayout()
+        add(topPanel, BorderLayout.NORTH)
+        add(JScrollPane(infoArea), BorderLayout.CENTER)
+        size = Dimension(600, 600)
+    }
+
+    fun show() {
+        mainFrame.isVisible = true
+    }
+}
---
+++ lessons/src/main/kotlin/lesson_65_coroutines/callbacks/Test.kt
@@ -0,0 +1,5 @@
+package lesson_65_coroutines.callbacks
+
+fun main() {
+    Display.show()
+}
