29abc9a5f447aa7e9735b22c6be42731ed7a6759
Mark Klinitskii <mark23klin@mail.ru>
2025-12-11 00:19:55 +0300

    Lesson 85 - Reactive Style

--- lessons/src/main/kotlin/lesson_83_dictionary/Display.kt
+++ lessons/src/main/kotlin/lesson_83_dictionary/Display.kt
@@ -2,10 +2,16 @@ package lesson_83_dictionary
 
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
 import java.awt.BorderLayout
 import java.awt.Font
+import java.awt.event.KeyAdapter
+import java.awt.event.KeyEvent
 import javax.swing.JButton
 import javax.swing.JFrame
 import javax.swing.JLabel
@@ -16,25 +22,25 @@ import javax.swing.JTextField
 
 object Display {
 
-    val scope = CoroutineScope(Dispatchers.IO + SupervisorJob())
-    val repository = Repository
+    private lateinit var queries: Flow<String>
+    private val scope = CoroutineScope(Dispatchers.IO + SupervisorJob())
+    private val repository = Repository
+    private var loadingJob: Job? = null
 
     private val enterWordLabel = JLabel("Enter word: ").apply {
         font = Font(Font.SANS_SERIF, Font.PLAIN, 17)
     }
     private val searchField = JTextField(20).apply {
         font = Font(Font.SANS_SERIF, Font.PLAIN, 17)
+        addKeyListener(object : KeyAdapter() {
+            override fun keyReleased(p0: KeyEvent?) {
+                loadDefinition()
+            }
+        })
     }
     private val searchButton = JButton("Search").apply {
         addActionListener {
-            scope.launch {
-                isEnabled = false
-                resultArea.text = "Loading..."
-                val word = searchField.text.trim()
-                val definition = repository.loadDefinition(word).joinToString("\n\n")
-                resultArea.text = definition.ifEmpty { "Not found" }
-                isEnabled = true
-            }
+            loadDefinition()
         }
     }
     private val resultArea = JTextArea(25, 50).apply {
@@ -55,10 +61,28 @@ object Display {
         pack()
     }
 
+    private fun loadDefinition() {
+
+    }
+
+
     fun show() {
         mainFrame.isVisible = true
     }
 
+    init {
+        queries.onEach {
+            searchButton.isEnabled = false
+            resultArea.text = "Loading..."
+        }.map {
+            repository.loadDefinition(it)
+        }.map {
+            it.joinToString("\n\n").ifEmpty { "Not found" }
+        }.onEach {
+            resultArea.text = it
+            searchButton.isEnabled = true
+        }.launchIn(scope)
+    }
 }
 
 fun main() {
